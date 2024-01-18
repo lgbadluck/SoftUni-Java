ed380835506429460d1ab181d9958491eee4317b
lgbadluck <lg.badluck@gmail.com>
2024-01-18 13:26:20 +0200

    100% in Judge for DayOfWeek_01.java in ArraysLab

--- ProgrammingFundamentals-Java/src/ArraysLab/DayOfWeek_01.java
+++ ProgrammingFundamentals-Java/src/ArraysLab/DayOfWeek_01.java
@@ -6,6 +6,20 @@ public class DayOfWeek_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] days = new String [8];
 
+        days[0]="Invalid day!";
+        days[1]="Monday";
+        days[2]="Tuesday";
+        days[3]="Wednesday";
+        days[4]="Thursday";
+        days[5]="Friday";
+        days[6]="Saturday";
+        days[7]="Sunday";
+
+        int dayNumber = Integer.parseInt(scanner.nextLine());
+
+        if (dayNumber >= 1 && dayNumber<=7) System.out.printf("%s\n", days[dayNumber]);
+        else System.out.printf("%s\n", days[0]);
     }
 }
