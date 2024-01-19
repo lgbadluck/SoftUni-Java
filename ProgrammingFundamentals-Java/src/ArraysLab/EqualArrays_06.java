c634f9fe07beed207b318378201babbbc978a80e
lgbadluck <lg.badluck@gmail.com>
2024-01-19 10:02:34 +0200

    100% in Judge for EqualArrays_06.java

--- ProgrammingFundamentals-Java/src/ArraysLab/EqualArrays_06.java
+++ ProgrammingFundamentals-Java/src/ArraysLab/EqualArrays_06.java
@@ -1,11 +1,25 @@
 package ArraysLab;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EqualArrays_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] firstArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(e->Integer.parseInt(e)).toArray();
+        int[] secondArr = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(e->Integer.parseInt(e)).toArray();
 
+        int sum = 0;
+        for (int i = 0; i < firstArr.length; i++) {
+            sum+=firstArr[i];
+            if(firstArr[i] != secondArr[i]) {
+                System.out.printf("Arrays are not identical. Found difference at %d index.\n", i);
+                break;
+            }
+            if(i== firstArr.length-1) {
+                System.out.printf("Arrays are identical. Sum: %d\n", sum);
+            }
+        }
     }
 }
