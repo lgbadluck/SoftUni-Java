e3d09963f318dea02f3216ebe05668f8d28757a6
lgbadluck <lg.badluck@gmail.com>
2024-01-19 10:31:52 +0200

    100% in Judge for CommonElements_02.java

--- ProgrammingFundamentals-Java/src/ArraysExercise/CommonElements_02.java
+++ ProgrammingFundamentals-Java/src/ArraysExercise/CommonElements_02.java
@@ -1,11 +1,22 @@
 package ArraysExercise;
 
+import javax.swing.*;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class CommonElements_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] firstArr = scanner.nextLine().split(" ");
+        String[] secondArr = scanner.nextLine().split(" ");
 
+        for (int i = 0; i < secondArr.length; i++) {
+            for (int j = 0; j < firstArr.length; j++) {
+                if (secondArr[i].equals(firstArr[j])) {
+                    System.out.print(secondArr[i] + " ");
+                }
+            }
+        }
     }
 }
