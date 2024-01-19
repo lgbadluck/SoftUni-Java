ff627d955d1acb7b4b1617cee982640dccd64887
lgbadluck <lg.badluck@gmail.com>
2024-01-19 14:12:49 +0200

    100% in Judge for PascalTriangle_02.java

--- ProgrammingFundamentals-Java/src/ArraysMoreExercise/PascalTriangle_02.java
+++ ProgrammingFundamentals-Java/src/ArraysMoreExercise/PascalTriangle_02.java
@@ -6,6 +6,25 @@ public class PascalTriangle_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        int[][] pascalTriangle = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i==0 || j==0) {
+                    pascalTriangle[i][j]=1;
+                } else pascalTriangle[i][j]=pascalTriangle[i-1][j]+pascalTriangle[i][j-1];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int tempIdx = i;
+            //(n+1)-pascalTriangle.length+i
+            for (int j = 0; j < (n+1)-pascalTriangle.length+i; j++) {
+                System.out.print(pascalTriangle[tempIdx--][j] + " ");
+            }
+            System.out.print("\n");
+        }
     }
 }
