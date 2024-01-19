7e5af95cdd9a16cf697425a0a0fdb96dc9a179d8
lgbadluck <lg.badluck@gmail.com>
2024-01-19 11:22:43 +0200

    100% in Judge for TopIntegers_05.java

--- ProgrammingFundamentals-Java/src/ArraysExercise/TopIntegers_05.java
+++ ProgrammingFundamentals-Java/src/ArraysExercise/TopIntegers_05.java
@@ -1,11 +1,25 @@
 package ArraysExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TopIntegers_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (i == numbers.length-1) {
+                System.out.print(numbers[i] + " ");
+                return;
+            }
+            for (int j = i+1; j < numbers.length; j++) {
+                if (!(numbers[i] > numbers[j])) break;
+                else if (j== numbers.length-1) System.out.print(numbers[i] + " ");
+            }
+
+        }
 
     }
 }
