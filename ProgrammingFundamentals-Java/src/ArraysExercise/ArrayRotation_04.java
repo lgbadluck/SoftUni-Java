a0364a235f5472f5ef233bdb70f07313b829e057
lgbadluck <lg.badluck@gmail.com>
2024-01-19 10:49:14 +0200

    100% in Judge for ArrayRotation_04.java

--- ProgrammingFundamentals-Java/src/ArraysExercise/ArrayRotation_04.java
+++ ProgrammingFundamentals-Java/src/ArraysExercise/ArrayRotation_04.java
@@ -1,11 +1,28 @@
 package ArraysExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ArrayRotation_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+        int rotations = Integer.parseInt(scanner.nextLine());
 
+        while (rotations>0) {
+            int temp = numbers[0];
+            if (rotations == numbers.length) break;
+            if (rotations > numbers.length) rotations=rotations-numbers.length;
+            for (int i = 0; i < numbers.length-1; i++) {
+                numbers[i] = numbers[i+1];
+            }
+            numbers[numbers.length-1]=temp;
+            rotations--;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
     }
 }
