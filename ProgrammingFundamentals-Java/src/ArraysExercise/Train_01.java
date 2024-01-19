499f095fd1f5c6ea5fe02ed3adfd823600785a1d
lgbadluck <lg.badluck@gmail.com>
2024-01-19 10:24:56 +0200

    100% in Judge for Train_01.java

--- ProgrammingFundamentals-Java/src/ArraysExercise/Train_01.java
+++ ProgrammingFundamentals-Java/src/ArraysExercise/Train_01.java
@@ -6,6 +6,16 @@ public class Train_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int sum = 0;
+        int n  = Integer.parseInt(scanner.nextLine());
+        int[] train = new int[n];
 
+        for (int i = 0; i < n; i++) {
+            train[i] = Integer.parseInt(scanner.nextLine());
+            System.out.print(train[i] + " ");
+            sum += train[i];
+        }
+
+        System.out.println("\n" + sum);
     }
 }
