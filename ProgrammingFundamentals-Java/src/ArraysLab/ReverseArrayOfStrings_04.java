a996e0e3d1bfa88713ec4b1043c760343d49097f
lgbadluck <lg.badluck@gmail.com>
2024-01-19 09:49:57 +0200

    100% in Judge for ReverseArrayOfStrings_04.java

--- ProgrammingFundamentals-Java/src/ArraysLab/ReverseArrayOfStrings_04.java
+++ ProgrammingFundamentals-Java/src/ArraysLab/ReverseArrayOfStrings_04.java
@@ -1,11 +1,22 @@
 package ArraysLab;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class ReverseArrayOfStrings_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] arrStr = scanner.nextLine().split(" ");
 
+        for (int i = 0; i < arrStr.length/2; i++) {
+            String temp = arrStr[i];
+            arrStr[i] = arrStr[arrStr.length-1-i];
+            arrStr[arrStr.length-1-i] = temp;
+        }
+
+        for (int i = 0; i < arrStr.length; i++) {
+            System.out.print(arrStr[i] + " ");
+        }
     }
 }
