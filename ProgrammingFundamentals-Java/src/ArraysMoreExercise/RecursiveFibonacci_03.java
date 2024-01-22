193cbc1fcb264a6b5e371b1273ac70443aeaceb4
lgbadluck <lg.badluck@gmail.com>
2024-01-22 09:15:21 +0200

    100% in Judge for RecursiveFibonacci_03.java

--- ProgrammingFundamentals-Java/src/ArraysMoreExercise/RecursiveFibonacci_03.java
+++ ProgrammingFundamentals-Java/src/ArraysMoreExercise/RecursiveFibonacci_03.java
@@ -3,9 +3,17 @@ package ArraysMoreExercise;
 import java.util.Scanner;
 
 public class RecursiveFibonacci_03 {
+    public static int GetFib(int x) {
+        if (x==1) return 1;
+        else if (x==2) return 1;
+        else return GetFib(x-1) + GetFib(x-2);
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int n = Integer.parseInt(scanner.nextLine());
 
+        System.out.println(GetFib(n));
     }
+
 }
