33869380e0d94d7b4bf9f1354d6e179552e0cb18
lgbadluck <lg.badluck@gmail.com>
2024-01-24 10:35:18 +0200

    Change Project Structure -> Language to Java11 and 100% in Judge for LadyBugs_06.java

--- ProgrammingFundamentals-Java/src/ArraysMoreExercise/LadyBugs_06.java
+++ ProgrammingFundamentals-Java/src/ArraysMoreExercise/LadyBugs_06.java
@@ -1,69 +1,61 @@
 package ArraysMoreExercise;
 
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.Arrays;
 
 public class LadyBugs_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
-        String fieldStr = scanner.nextLine();
         int[] fieldNumArr = new int[n];
-
-        //System.out.print("BEFORE fieldNumArr: ");
-        String[] fieldStrArr = fieldStr.split("\\s+");
+        String[] fieldStrArr = scanner.nextLine().split(" ");
         for (int i = 0; i < fieldStrArr.length; i++) {
-            fieldNumArr[i] = Integer.parseInt(fieldStrArr[i]);
-            if (fieldNumArr[i] > 0) fieldNumArr[i] = 1;
-            //System.out.print(fieldNumArr[i] + " ");
+            int index = Integer.parseInt(fieldStrArr[i]);
+            if (index >= 0 && index < fieldNumArr.length) fieldNumArr[index] = 1;
         }
 
 
         String command = scanner.nextLine();
         while(!command.equals("end")) {
-            String[] commandArr = command.split("\\s+");
-            /*
-            System.out.println();
-            for (int i = 0; i < commandArr.length; i++) {
-                System.out.printf("%s ", commandArr[i]);
-            }
-            System.out.println();
-            */
+            String[] commandArr = command.split(" ");
             int position = Integer.parseInt(commandArr[0]);
             String direction = commandArr[1];
             int distance = Integer.parseInt(commandArr[2]);
 
-            if (fieldNumArr[position]==1) {
-                fieldNumArr[position] = 0;
-                switch (direction) {
-                    case "right":
-                        do{
-                            position+=distance;
-                            if(position<0 || position>n-1) {
-                                break;
-                            }
-                        } while (fieldNumArr[position]!=0);
-                        if(!(position<0 || position>n-1)) fieldNumArr[position]++;
-                        break;
-                    case "left":
-                        do{
-                            position-=distance;
-                            if(position<0 || position>n-1) {
-                                break;
-                            }
-                        } while (fieldNumArr[position]!=0);
-                        if(!(position<0 || position>n-1)) fieldNumArr[position]++;
-                        break;
-                    default:
-                        System.out.printf("Error - direction unknown!");
-                        break;
+            if( position<0 || position>fieldNumArr.length-1 || fieldNumArr[position]==0) {
+                command = scanner.nextLine();
+                continue;
+            }
+
+            fieldNumArr[position]=0;
+
+            if (direction.equals("right")) {
+                position+=distance;
+                if (!(position<0 || position>fieldNumArr.length-1) && fieldNumArr[position]==1){
+                    while (fieldNumArr[position]==1) {
+                        position+=distance;
+                        if( position<0 || position>fieldNumArr.length-1 ) break;
+                    }
+                }
+                if ( !(position<0 || position>fieldNumArr.length-1) ) {
+                    fieldNumArr[position]=1;
+                }
+            } else if (direction.equals("left")) {
+                position-=distance;
+                if (!(position<0 || position>fieldNumArr.length-1) && fieldNumArr[position]==1){
+                    while (fieldNumArr[position]==1) {
+                        position-=distance;
+                        if( position<0 || position>fieldNumArr.length-1 ) break;
+                    }
+                }
+                if ( !(position<0 || position>fieldNumArr.length-1) ) {
+                    fieldNumArr[position]=1;
                 }
             }
+
             command = scanner.nextLine();
         }
-
-        //System.out.print("fieldNumArr: ");
         System.out.print("\n");
         for (int i = 0; i < n; i++) {
             System.out.print(fieldNumArr[i] + " ");
