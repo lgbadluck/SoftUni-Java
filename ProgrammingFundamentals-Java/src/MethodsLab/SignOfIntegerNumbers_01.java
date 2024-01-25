362ede40449a50a2bc087d4d35e2c5287baf0ae4
lgbadluck <lg.badluck@gmail.com>
2024-01-25 09:05:58 +0200

    100% in Judge for SignOfIntegerNumbers_01.java

--- ProgrammingFundamentals-Java/src/MethodsLab/SignOfIntegerNumbers_01.java
+++ ProgrammingFundamentals-Java/src/MethodsLab/SignOfIntegerNumbers_01.java
@@ -6,6 +6,16 @@ public class SignOfIntegerNumbers_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.parseInt(scanner.nextLine());
+        getSignOfInt(num);
+    }
+    public static void getSignOfInt(int param1)
+    {
+        String sign = "";
+        if(param1 > 0) sign = "positive";
+        else if(param1<0) sign = "negative";
+        else sign = "zero";
 
+        System.out.printf("The number %d is %s.\n", param1, sign);
     }
 }
