26ec2ae886c36e01c2a4eb381f344aa93b5164e0
lgbadluck <lg.badluck@gmail.com>
2024-01-26 09:34:26 +0200

    100% in Judge for SmallestOfThreeNumbers_01.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/SmallestOfThreeNumbers_01.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/SmallestOfThreeNumbers_01.java
@@ -6,6 +6,22 @@ public class SmallestOfThreeNumbers_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
+        System.out.println(getSmallestOf3(a,b,c));
+    }
+    public static int getSmallestOf3(int a, int b, int c)
+    {
+        int min = a;
+        if(b<a) {
+            if (c<b) min= c;
+            else min=b;
+        }
+        else if (c<a) {
+            if (c<b) min= c;
+            else min=b;
+        }
+        return min;
     }
 }
