c0329395f525f00560bd9d16587aab3376b8ff65
lgbadluck <lg.badluck@gmail.com>
2024-01-26 10:03:31 +0200

    100% in Judge for CharactersInRange_03.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/CharactersInRange_03.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/CharactersInRange_03.java
@@ -6,6 +6,21 @@ public class CharactersInRange_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        char start = scanner.nextLine().charAt(0);
+        char end = scanner.nextLine().charAt(0);
 
+        printCharsInRange(start, end);
+    }
+
+    public static void printCharsInRange(char start, char end) {
+        if (start > end) {
+            char temp = start;
+            start = end;
+            end = temp;
+        }
+        for (int i = start + 1; i < end; i++) {
+            System.out.print((char) i + " ");
+        }
+        System.out.println();
     }
 }
