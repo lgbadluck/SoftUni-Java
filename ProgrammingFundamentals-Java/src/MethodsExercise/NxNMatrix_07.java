d405dec0b78dff628c4c7c307569b52bf779edeb
lgbadluck <lg.badluck@gmail.com>
2024-01-26 11:09:53 +0200

    100% in Judge for NxNMatrix_07.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/NxNMatrix_07.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/NxNMatrix_07.java
@@ -6,6 +6,17 @@ public class NxNMatrix_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num = Integer.parseInt(scanner.nextLine());
 
+        printNxN(num);
+    }
+    public static void printNxN(int n)
+    {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print(n + " ");
+            }
+            System.out.println();
+        }
     }
 }
