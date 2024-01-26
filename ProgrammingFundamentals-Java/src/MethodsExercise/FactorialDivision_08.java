a616092ea675b95c9a508943b610fef08af5492d
lgbadluck <lg.badluck@gmail.com>
2024-01-26 11:15:54 +0200

    100% in Judge for FactorialDivision_08.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/FactorialDivision_08.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/FactorialDivision_08.java
@@ -6,6 +6,21 @@ public class FactorialDivision_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
 
+        System.out.printf("%.2f\n", getFactorialDivision(num1,num2));
+    }
+    public static double getFactorialDivision(int n1, int n2)
+    {
+        return getFactorial(n1)/getFactorial(n2);
+    }
+    public static double getFactorial(int numToFact)
+    {
+        double result=1;
+        for (int i = 2; i <= numToFact; i++) {
+            result*=i;
+        }
+        return result;
     }
 }
