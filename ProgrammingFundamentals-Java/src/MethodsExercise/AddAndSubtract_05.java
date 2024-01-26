a61dae1ddc18b3b0433621eb0d124728dd599b44
lgbadluck <lg.badluck@gmail.com>
2024-01-26 10:49:03 +0200

    100% in Judge for AddAndSubtract_05.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/AddAndSubtract_05.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/AddAndSubtract_05.java
@@ -6,6 +6,22 @@ public class AddAndSubtract_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+        int c = Integer.parseInt(scanner.nextLine());
 
+        System.out.println(addAndSubtract(a,b,c));
+    }
+    public static int sum (int a, int b)
+    {
+        return a+b;
+    }
+    public static int subtract (int a, int b)
+    {
+        return a-b;
+    }
+    public static int addAndSubtract (int a, int b, int c)
+    {
+        return subtract(sum(a,b),c);
     }
 }
