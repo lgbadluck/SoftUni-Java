f55e46c8cfdc1a768d9293b6990a4da524b85580
lgbadluck <lg.badluck@gmail.com>
2024-01-26 11:06:41 +0200

    100% in Judge for MiddleCharacters_06.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/MiddleCharacters_06.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/MiddleCharacters_06.java
@@ -6,6 +6,17 @@ public class MiddleCharacters_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String input = scanner.nextLine();
 
+        printMiddleChars(input);
+    }
+
+    public static void printMiddleChars(String inputStr) {
+        int len =inputStr.length();
+        if (len% 2 == 0) {
+            System.out.printf("%c%c",inputStr.charAt(len/2-1), inputStr.charAt(len/2));
+        } else {
+            System.out.println(inputStr.charAt(len/2));
+        }
     }
 }
