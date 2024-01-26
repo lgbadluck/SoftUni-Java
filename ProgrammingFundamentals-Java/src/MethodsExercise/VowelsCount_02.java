af606ecb4f9aca750bf303f23a55833dc3ed9d52
lgbadluck <lg.badluck@gmail.com>
2024-01-26 09:43:30 +0200

    100% in Judge for VowelsCount_02.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/VowelsCount_02.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/VowelsCount_02.java
@@ -6,6 +6,20 @@ public class VowelsCount_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-
+        String inputStr = scanner.nextLine();
+        System.out.println(getVowelsCount(inputStr));
+    }
+    public static int getVowelsCount(String input)
+    {
+        int sum=0;
+        for (int i = 0; i < input.length(); i++) {
+            char currChar = input.toLowerCase().charAt(i);
+            if (currChar == 'e' || currChar == 'i' ||
+                    currChar == 'o' || currChar == 'u' ||
+                    currChar == 'a' ) {
+                sum++;
+            }
+        }
+        return sum;
     }
 }
