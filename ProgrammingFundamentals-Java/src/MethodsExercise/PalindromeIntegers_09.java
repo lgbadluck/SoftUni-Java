915b95dc6cd545f39d7c3a628a690b705c26a78a
lgbadluck <lg.badluck@gmail.com>
2024-01-26 11:44:41 +0200

    100% in Judge for PalindromeIntegers_09.java

--- ProgrammingFundamentals-Java/src/MethodsExercise/PalindromeIntegers_09.java
+++ ProgrammingFundamentals-Java/src/MethodsExercise/PalindromeIntegers_09.java
@@ -5,7 +5,25 @@ import java.util.Scanner;
 public class PalindromeIntegers_09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
 
-
+        while(!input.equals("END") ){
+            //int num = Integer.parseInt(input);
+            checkPalindromeInteger(input);
+            input = scanner.nextLine();
+        }
+    }
+    public static void checkPalindromeInteger(String n)
+    {
+        int len = n.length();
+        for (int i = 0; i < len/2; i++) {
+            char thisChar = n.charAt(i);
+            char mirrorChar = n.charAt(len-1-i);
+            if (thisChar!=mirrorChar) {
+                System.out.println("false");
+                return;
+            }
+        }
+        System.out.println("true");
     }
 }
