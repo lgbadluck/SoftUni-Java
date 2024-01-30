5274172175f9b5ed31d89e8148aad017bac03038
lgbadluck <lg.badluck@gmail.com>
2024-01-30 09:35:18 +0200

    100% in Judge for GaussTrick_02.java

--- ProgrammingFundamentals-Java/src/ListsLab/GaussTrick_02.java
+++ ProgrammingFundamentals-Java/src/ListsLab/GaussTrick_02.java
@@ -1,11 +1,31 @@
 package ListsLab;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class GaussTrick_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        List<Double> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
+        int size = numbers.size();
+        for (int i = 0; i < size/2; i++) {
+            numbers.set(i, ( numbers.get(i) + numbers.get(size-1-i)) );
+            numbers.remove(size-1-i);
+        }
+        System.out.println(joinElementsByDelimiter(numbers, " "));
+    }
+
+    public static String joinElementsByDelimiter(List<Double> items, String delimiter) {
+        String output = "";
+        for (Double item : items)
+            output += (new DecimalFormat("0.#").format(item) + delimiter);
+        return output;
     }
 }
