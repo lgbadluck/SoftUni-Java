9cde14d0e6b6c50981592a38069346659ca5947a
lgbadluck <lg.badluck@gmail.com>
2024-01-30 09:22:27 +0200

    100% in Judge for SumAdjacentEqualNumbers_01.java

--- ProgrammingFundamentals-Java/src/ListsLab/SumAdjacentEqualNumbers_01.java
+++ ProgrammingFundamentals-Java/src/ListsLab/SumAdjacentEqualNumbers_01.java
@@ -1,11 +1,36 @@
 package ListsLab;
 
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class SumAdjacentEqualNumbers_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        List<Double> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Double::parseDouble)
+                .collect(Collectors.toList());
+
+
+        for (int i = 0; i < numbers.size() - 1; i++) {
+            if (numbers.get(i).equals(numbers.get(i + 1))) {
+                numbers.set(i, (numbers.get(i) + numbers.get(i + 1)));
+                numbers.remove(i+1);
+                i=-1;
+            }
+        }
+
+        String output = joinElementsByDelimiter(numbers, " ");
+        System.out.println(output);
+    }
+
+    public static String joinElementsByDelimiter(List<Double> items, String delimiter) {
+        String output = "";
+        for (Double item : items)
+            output += (new DecimalFormat("0.#").format(item) + delimiter);
+        return output;
     }
 }
