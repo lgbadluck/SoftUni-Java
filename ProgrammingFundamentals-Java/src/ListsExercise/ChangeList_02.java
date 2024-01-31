49a4d642648f42d1f1b3ca7bb9c8821d796ad3da
lgbadluck <lg.badluck@gmail.com>
2024-01-31 10:17:34 +0200

    100% in Judge for ChangeList_02.java

--- ProgrammingFundamentals-Java/src/ListsExercise/ChangeList_02.java
+++ ProgrammingFundamentals-Java/src/ListsExercise/ChangeList_02.java
@@ -1,11 +1,36 @@
 package ListsExercise;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class ChangeList_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        //System.out.println(train.toString().replaceAll("[\\[\\],]", ""));
+
+        String[] input = scanner.nextLine().split(" ");
+
+        while (!input[0].equals("end")){
+            String cmd1 = input[0];
+
+            if (cmd1.equals("Delete")) {
+                int element = Integer.parseInt(input[1]);
+                numbers.remove((Integer) element);
+            } else if (cmd1.equals("Insert")) {
+                int element = Integer.parseInt(input[1]);
+                int index = Integer.parseInt(input[2]);
+                numbers.add(index, (Integer) element);
+            }
+
+            input = scanner.nextLine().split(" ");;
+        }
+        System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
     }
 }
