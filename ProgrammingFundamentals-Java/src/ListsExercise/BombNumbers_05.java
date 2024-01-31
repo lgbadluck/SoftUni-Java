d3eb0c91fc7c0fd671540cb6a3fb2891c4618f5a
lgbadluck <lg.badluck@gmail.com>
2024-01-31 12:23:45 +0200

    100% in Judge for BombNumbers_05.java

--- ProgrammingFundamentals-Java/src/ListsExercise/BombNumbers_05.java
+++ ProgrammingFundamentals-Java/src/ListsExercise/BombNumbers_05.java
@@ -13,9 +13,27 @@ public class BombNumbers_05 {
                 .map(Integer::parseInt)
                 .collect(Collectors.toList());
 
-        int bombNum = Integer.parseInt(scanner.nextLine());
-        int bombPower = Integer.parseInt(scanner.nextLine());
+        String[] bombInputs = scanner.nextLine().split(" ");
+        int bombNum = Integer.parseInt(bombInputs[0]);
+        int bombPower = Integer.parseInt(bombInputs[1]);
 
         //System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+
+        while (numbers.contains(bombNum)) {
+            int idx = numbers.indexOf(bombNum);
+            int start = idx - bombPower;
+            int end = idx + bombPower;
+            if(start<0) start = 0;
+            if(end>numbers.size()-1) end = numbers.size()-1;
+            for (int i = end; i >= start; i--) {
+                numbers.remove(i);
+            }
+        }
+        //System.out.println(numbers.toString().replaceAll("[\\[\\],]", ""));
+        int sum =0;
+        for (int i = 0; i < numbers.size(); i++) {
+            sum+=numbers.get(i);
+        }
+        System.out.println(sum);
     }
 }
