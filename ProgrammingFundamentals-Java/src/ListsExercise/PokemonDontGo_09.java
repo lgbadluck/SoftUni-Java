49759d55d0706e31439dbc10280cb867c0b4d2da
lgbadluck <lg.badluck@gmail.com>
2024-02-01 21:35:01 +0200

    100% in judge for PokemonDontGo_09.java

--- ProgrammingFundamentals-Java/src/ListsExercise/PokemonDontGo_09.java
+++ ProgrammingFundamentals-Java/src/ListsExercise/PokemonDontGo_09.java
@@ -1,11 +1,46 @@
 package ListsExercise;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class PokemonDontGo_09 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        int sumRemoved = 0;
+        while (!numbers.isEmpty()) {
+            //System.out.println(numbers);
+            int idx = Integer.parseInt(scanner.nextLine());
+
+            int valueBeRemoved = 0; //numbers.get(idx);
+            if (idx < 0) {
+                valueBeRemoved = numbers.get(0);
+                sumRemoved += valueBeRemoved;
+                numbers.set(0, numbers.get(numbers.size() - 1));
+            } else if (idx >= numbers.size()) {
+                valueBeRemoved = numbers.get(numbers.size() - 1);
+                sumRemoved += valueBeRemoved;
+                numbers.set((numbers.size() - 1), numbers.get(0));
+            } else {
+                valueBeRemoved = numbers.get(idx);
+                sumRemoved += valueBeRemoved;
+                numbers.remove(idx);
+            }
+
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i) > valueBeRemoved) {
+                    numbers.set(i, numbers.get(i) - valueBeRemoved);
+                } else {
+                    numbers.set(i, numbers.get(i) + valueBeRemoved);
+                }
+            }
+        }
+        System.out.println(sumRemoved);
     }
 }
