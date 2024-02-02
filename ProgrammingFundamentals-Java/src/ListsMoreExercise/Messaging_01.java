082b8c3da06bda011ed7f900adff3033f92c21b1
lgbadluck <lg.badluck@gmail.com>
2024-02-02 09:56:48 +0200

    100% in Judge for Messaging_01.java

--- ProgrammingFundamentals-Java/src/ListsMoreExercise/Messaging_01.java
+++ ProgrammingFundamentals-Java/src/ListsMoreExercise/Messaging_01.java
@@ -1,11 +1,41 @@
 package ListsMoreExercise;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Messaging_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        List<Integer> numbers = Arrays.stream(scanner.nextLine().split(" "))
+                .map(Integer::parseInt)
+                .collect(Collectors.toList());
+
+        StringBuilder text = new StringBuilder(scanner.nextLine());
+        String output = "";
+
+        for (int i = 0; i < numbers.size(); i++) {
+            int num = numbers.get(i);
+            int sum = 0;
+            while (num != 0) {
+                int modulo = num % 10;
+                sum += modulo;
+                num /= 10;
+            }
+            output = output + popGivenCharFromString(sum, text);
+        }
+        System.out.println(output);
+    }
+
+    public static String popGivenCharFromString(int idx, StringBuilder text) {
+        int idxInString = 0;
+        if (idx > text.length() - 1) idxInString = idx % text.length();
+        else idxInString = idx;
+        String result = "";
+        result = "" + text.charAt(idxInString);
+        text.deleteCharAt(idxInString);
+        return result;
     }
 }
