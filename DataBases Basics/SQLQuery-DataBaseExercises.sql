61165c4f07c77942556a12d770c3a8160eff9715
lgbadluck <lg.badluck@gmail.com>
2024-02-05 14:24:17 +0200

    Added new SQLQueries in MicrosoftSQLServer exercise

--- DataBases Basics/SQLQuery-DataBaseExercises.sql
+++ DataBases Basics/SQLQuery-DataBaseExercises.sql
@@ -326,3 +326,126 @@ JOIN Sales.OrderDetails AS od ON p.productid = od.productid
 GROUP BY p.categoryid
 HAVING AVG(qty) > 20
 
+SELECT empid, COUNT(*) AS cnt
+FROM Sales.Orders
+GROUP BY empid 
+--ORDER BY empid
+
+SELECT empid, COUNT(*) AS cnt
+FROM Sales.Orders
+GROUP BY empid WITH ROLLUP
+
+SELECT empid, YEAR(orderdate) as YR, COUNT(*) AS cnt
+FROM Sales.Orders
+GROUP BY empid, YEAR(orderdate) WITH ROLLUP
+
+SELECT empid, YEAR(orderdate) as YR, COUNT(*) AS cnt
+FROM Sales.Orders
+GROUP BY empid, YEAR(orderdate) WITH CUBE
+
+SELECT empid, YEAR(orderdate) as YR, COUNT(*) AS cnt
+FROM Sales.Orders
+GROUP BY GROUPING SETS (	empid,YEAR(orderdate),	());
+
+SELECT empid, YEAR(orderdate) as YR, COUNT(*) AS cnt
+FROM Sales.Orders
+GROUP BY GROUPING SETS (
+	(empid,YEAR(orderdate)),
+	(empid),
+	(YEAR(orderdate)),
+	());
+
+	--WORKING WITH DATAYPES
+SELECT 1 + '2' AS result
+SELECT '1' + 2 AS result
+SELECT '1' + '2' AS result
+SELECT 1 + 2 AS result
+
+--THIS WILL FAIL
+SELECT 1 + 'abc' AS result
+
+SELECT CAST(12 AS VARCHAR(10)) + 'abc' AS result
+SELECT CONVERT(VARCHAR(10), 12) + 'abc' AS result
+
+--TRY_CAST (expression AS data_type [(length)] )
+SELECT TRY_CAST ('13/31/2012' AS date) AS YearEndDate;
+SELECT TRY_CONVERT (date, '13/31/2012', 101) AS YearEndDateUS;
+
+DECLARE @dtvalue DATETIME
+SET @dtvalue = '2007-05-08 12:35:29.123';
+SELECT CONVERT (varchar(16), @dtvalue, 120)
+
+SELECT PARSE('12/31/2012' AS date) AS YearEndDateUS;
+--ERROR --SELECT PARSE('31/12/2012' AS date USING 'en-US') AS YearEndDate;
+SELECT PARSE('31/12/2012' AS date USING 'en-GB') AS YearEndDateUK;
+
+SELECT TRY_PARSE('12/31/2012' AS date) AS YearEndDateUS;
+SELECT TRY_PARSE('31/12/2012' AS date USING 'en-US') AS YearEndDate; --NULL not error like when using PARSE directly
+SELECT TRY_PARSE('31/12/2012' AS date USING 'en-GB') AS YearEndDateUK;
+
+SELECT empid, lastname, firstname, firstname + N' ' + lastname AS fullname --N is for National UTF encode of the string in ' ' (space in the example)
+FROM HR.Employees
+
+
+--ISDATE
+DECLARE @Name nvarchar(50) = 'XY14822'
+DECLARE @DiscontinuedDate datetime = '12/4/2012'
+SELECT ISDATE(@Name) AS NameISDATE,
+	ISDATE(@DiscontinuedDate) AS DiscontinuedDateISDATE;
+
+--ISNUMERIC
+DECLARE @Name nvarchar(50) = 'XY14822'
+DECLARE @DiscontinuedDate datetime = '12/4/2012'
+DECLARE @DayToManufacture int = 100
+SELECT ISNUMERIC(@Name) AS NameIISNUMERIC,
+	ISNUMERIC(@DiscontinuedDate) AS DiscontinuedDateISNUMERIC,
+	ISNUMERIC(@DayToManufacture) AS DaysToManucatureISNUMERIC;
+
+--Use string functions in a query
+SELECT SUBSTRING('Microsoft SQL Server',11,3);
+SELECT LEFT('Microsoft SQL Server',9);
+SELECT RIGHT('Microsoft SQL Server',6);
+SELECT LEN('Microsoft SQL Server                '); --20
+SELECT DATALENGTH('Microsoft SQL Server       '); --25 Why is it different from LEN? (counts ' ')
+SELECT CHARINDEX('SQL','Microsoft SQL Server'); --try with %SQL% before showing PATINDEX
+SELECT PATINDEX('%SQL%','Microsoft SQL Server');
+SELECT REPLACE('Microsoft SQL Server Denali', 'Denali', '2012');
+SELECT UPPER('Microsoft SQL Server');
+SELECT LOWER('Microsoft SQL Server');
+SELECT REPLICATE('MARKETBRIDGE ', 5);
+
+--Date and Time DATA TYPES
+--Display various current date and time functions
+
+SELECT
+	GETDATE()				AS [GetDate],
+	CURRENT_TIMESTAMP		AS [Current_Timestamp],
+	GETUTCDATE()			AS [GetUTCDate],
+	SYSDATETIME()			AS [SYSDateTime],
+	SYSUTCDATETIME()		AS [SYSUTCDateTime],
+	SYSDATETIMEOFFSET()		AS [SYSDateTimeOffset];
+
+--Display Various functions which return a portion of date or time
+SELECT DATENAME(year, '20120212');
+SELECT DAY('20120212') AS [Day],
+		MONTH('20120212') AS [Month],
+		YEAR('20120212') AS [Year];
+
+--Display various functions which return a date or time from parts
+SELECT DATETIMEFROMPARTS(2012,2,12,8,30,0,0) AS Result; --7
+SELECT DATETIME2FROMPARTS(2012,2,12,8,30,0,0,0) AS Result; --8
+
+--Demonstrate DATEDIFF with this to show difference in precision:
+--Shows the difference between two dates (measure, start, end)
+SELECT DATEDIFF(millisecond, GETDATE(), SYSDATETIME());
+SELECT DATEDIFF(day, '2008-06-05', '2008-08-05') AS DifferenceInDays
+
+--Use ISDATE to check validity of inputs:
+SELECT ISDATE('20120212'); --is valid
+SELECT ISDATE('20120230'); --February doesn't have 30 days
+
+DECLARE @STARTDATE DATETIME = GETDATE()
+SELECT EOMONTH (@STARTDATE) AS ThisMonthLastDay
+SELECT EOMONTH (@STARTDATE, 1) AS NEXTMonthLastDay
+SELECT EOMONTH (@STARTDATE, -1) AS PREVIOUSMonthLastDay
+
