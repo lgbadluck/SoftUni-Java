b7f2571e221a5d62b7bcb43cc77655859908b718
lgbadluck <lg.badluck@gmail.com>
2024-02-06 09:14:13 +0200

    100% in Judge for SumBigNumbers_02.java

--- ProgrammingFundamentals-Java/src/ObjectsAndClassesLab/SumBigNumbers_02.java
+++ ProgrammingFundamentals-Java/src/ObjectsAndClassesLab/SumBigNumbers_02.java
@@ -1,11 +1,17 @@
 package ObjectsAndClassesLab;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class SumBigNumbers_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        BigInteger firstNumber = new BigInteger(scanner.nextLine());
+        BigInteger secondNumber = new BigInteger(scanner.nextLine());
+
+        BigInteger sumBigInt = firstNumber.add(secondNumber);
+
+        System.out.println(sumBigInt);
     }
 }
