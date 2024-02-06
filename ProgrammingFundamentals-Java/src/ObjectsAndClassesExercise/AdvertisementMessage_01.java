492e50ba905a2f9f8443de9cda90d5dea00935bd
lgbadluck <lg.badluck@gmail.com>
2024-02-06 10:19:39 +0200

    100% in Judge for AdvertisementMessage_01.java

--- ProgrammingFundamentals-Java/src/ObjectsAndClassesExercise/AdvertisementMessage_01.java
+++ ProgrammingFundamentals-Java/src/ObjectsAndClassesExercise/AdvertisementMessage_01.java
@@ -1,11 +1,64 @@
 package ObjectsAndClassesExercise;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class AdvertisementMessage_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String[] phrases = {
+                "Excellent product.",
+                "Such a great product.",
+                "I always use that product.",
+                "Best product of its category.",
+                "Exceptional product.",
+                "I can’t live without this product.",
+        };
+
+        String[] events = {
+                "Now I feel good.",
+                "I have succeeded with this product.",
+                "Makes miracles. I am happy of the results!",
+                "I cannot believe but now I feel awesome.",
+                "Try it yourself, I am very satisfied.",
+                "I feel great!",
+        };
+        String[] authors = {
+                "Diana",
+                "Petya",
+                "Stella",
+                "Elena",
+                "Katya",
+                "Iva",
+                "Annie",
+                "Eva",
+        };
+        String[] cities = {
+                "Burgas",
+                "Sofia",
+                "Plovdiv",
+                "Varna",
+                "Ruse",
+
+        };
+
         int n = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < n; i++) {
+            System.out.printf("%s %s %s - %s\n",
+                    getRandomWord(phrases),
+                    getRandomWord(events),
+                    getRandomWord(authors),
+                    getRandomWord(cities)
+                    );
+        }
+    }
+    public static String getRandomWord (String[] words) {
+        Random random = new Random();
+
+        int randomIdx = random.nextInt(words.length);
+
+        return words[randomIdx];
     }
 }
