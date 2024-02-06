130183492163d8efcaef523fbe56315e244ed98f
lgbadluck <lg.badluck@gmail.com>
2024-02-06 09:11:33 +0200

    100% in Judge for RandomizeWords_01.java

--- ProgrammingFundamentals-Java/src/ObjectsAndClassesLab/RandomizeWords_01.java
+++ ProgrammingFundamentals-Java/src/ObjectsAndClassesLab/RandomizeWords_01.java
@@ -1,11 +1,30 @@
 package ObjectsAndClassesLab;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.Random;
 
 public class RandomizeWords_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] words = scanner.nextLine().split(" ");
+        printWordsInRandomOrder(words);
+    }
+
+    public static void printWordsInRandomOrder (String[] words) {
+        Random random = new Random();
+
+        List<String> wordsList = new LinkedList<>();
+        wordsList.addAll(List.of(words));
+        //System.out.println(wordsList);
+
+        for (int i = 0; i < words.length; i++) {
+            int randomIdx = random.nextInt(wordsList.size());
+            String thisWord = wordsList.get(randomIdx);
+            System.out.println(thisWord);
+            wordsList.remove(randomIdx);
+        }
     }
 }
