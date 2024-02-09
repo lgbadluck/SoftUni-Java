dc43b259163efc83b3bee1bb7b256ab9fb2a8c1d
lgbadluck <lg.badluck@gmail.com>
2024-02-09 13:46:09 +0200

    laraval-php more files

---
+++ laravel-php-project/demoproj/resources/views/hello.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LARAVEL Template</title>
+</head>
+<body>
+    This is LARAVEL Template
+    <br>
+    
+    Hello {{$name['fname']}} {{$name['lname']}}!
+    
+</body>
+</html>
