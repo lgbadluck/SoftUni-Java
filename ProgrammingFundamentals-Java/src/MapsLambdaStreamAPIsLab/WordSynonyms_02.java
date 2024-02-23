39da49b31deb126237a9af75992baf4662227c62
lgbadluck <lg.badluck@gmail.com>
2024-02-23 12:04:52 +0200

    100% in Judge for WordSynonyms_02.java

--- ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsLab/WordSynonyms_02.java
+++ ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsLab/WordSynonyms_02.java
@@ -1,11 +1,29 @@
 package MapsLambdaStreamAPIsLab;
 
+import java.util.LinkedHashMap;
+import java.util.ArrayList;
+import java.util.Map;
 import java.util.Scanner;
 
 public class WordSynonyms_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        LinkedHashMap<String, ArrayList<String>> wordSynonyms = new LinkedHashMap<>();
         int n = Integer.parseInt(scanner.nextLine());
+
+        while (n>0) {
+            String word = scanner.nextLine();
+            String synonym = scanner.nextLine();
+
+            wordSynonyms.putIfAbsent(word, new ArrayList<>());
+            wordSynonyms.get(word).add(synonym);
+
+            n--;
+        }
+
+        for (Map.Entry<String, ArrayList<String>> entry : wordSynonyms.entrySet()) {
+            System.out.printf("%s - %s\n", entry.getKey(), entry.getValue().toString().replace("[","").replace("]",""));
+        }
     }
 }
