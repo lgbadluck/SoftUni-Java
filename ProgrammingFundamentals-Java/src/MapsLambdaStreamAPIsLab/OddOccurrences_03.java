c2c68ec98a44a941cc9538b1e6fa7f1d88447d55
lgbadluck <lg.badluck@gmail.com>
2024-02-23 12:25:02 +0200

    100% in Judge for OddOccurrences_03.java

--- ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsLab/OddOccurrences_03.java
+++ ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsLab/OddOccurrences_03.java
@@ -1,11 +1,26 @@
 package MapsLambdaStreamAPIsLab;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class OddOccurrences_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] wordsStrings = scanner.nextLine().toLowerCase().split(" ");
+
+        LinkedHashMap<String, Integer> wordsMap = new LinkedHashMap<>();
+        for (String word : wordsStrings) {
+            if (!wordsMap.containsKey(word)) {
+                wordsMap.put(word, 0);
+            }
+            wordsMap.put(word, wordsMap.get(word) + 1);
+        }
+
+        List<String> oddOccurrences = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordsMap.entrySet()) {
+            if (entry.getValue() % 2 == 1) oddOccurrences.add(entry.getKey());
+        }
+
+        System.out.println(oddOccurrences.toString().replace("[","").replace("]",""));
     }
 }
