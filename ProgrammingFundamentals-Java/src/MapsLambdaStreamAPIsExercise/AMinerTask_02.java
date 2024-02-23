8d669510c1f7bccc93cd6990eba48e79e3720fcd
lgbadluck <lg.badluck@gmail.com>
2024-02-23 15:13:29 +0200

    100% in Judge for AMinerTask_02.java

--- ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsExercise/AMinerTask_02.java
+++ ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsExercise/AMinerTask_02.java
@@ -1,11 +1,30 @@
 package MapsLambdaStreamAPIsExercise;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class AMinerTask_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        LinkedHashMap<String, Double> minersMap = new LinkedHashMap<>();
+
+        String input = scanner.nextLine();
+        while(!input.equals("stop")) {
+            String resource = input;
+            double quantity = Double.parseDouble(scanner.nextLine());
+
+            if(!minersMap.containsKey(resource)) {
+                minersMap.put(resource, 0.0);
+            }
+            minersMap.put(resource, minersMap.get(resource) + quantity);
+
+            input = scanner.nextLine();
+        }
+
+        for (Map.Entry<String, Double> entry : minersMap.entrySet()) {
+            System.out.printf("%s -> %.0f\n", entry.getKey(), entry.getValue() );
+        }
     }
 }
