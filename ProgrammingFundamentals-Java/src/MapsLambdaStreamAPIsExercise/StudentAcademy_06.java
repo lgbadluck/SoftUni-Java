983f8f0b18f149e2a28f460558dd29ce1e473a8e
lgbadluck <lg.badluck@gmail.com>
2024-02-23 16:53:17 +0200

    100% in Judge for StudentAcademy_06.java

--- ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsExercise/StudentAcademy_06.java
+++ ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsExercise/StudentAcademy_06.java
@@ -1,5 +1,8 @@
 package MapsLambdaStreamAPIsExercise;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class StudentAcademy_06 {
@@ -7,5 +10,23 @@ public class StudentAcademy_06 {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+
+        LinkedHashMap<String, ArrayList<Double>> studentGradesMap = new LinkedHashMap<>();
+
+        while (n>0) {
+            String studentName = scanner.nextLine();
+            Double thisGrade = Double.parseDouble(scanner.nextLine());
+
+            studentGradesMap.putIfAbsent(studentName, new ArrayList<>());
+            studentGradesMap.get(studentName).add(thisGrade);
+
+            n--;
+        }
+
+        //System.out.println(studentGradesMap.toString());
+        for (Map.Entry<String, ArrayList<Double>> entry : studentGradesMap.entrySet()) {
+            double average = entry.getValue().stream().mapToDouble(Double::doubleValue).average().orElse(0.0);
+            if (average >= 4.50) System.out.printf("%s -> %.2f\n", entry.getKey(), average);
+        }
     }
 }
