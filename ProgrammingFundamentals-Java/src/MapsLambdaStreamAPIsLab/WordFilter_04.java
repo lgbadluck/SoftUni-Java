de34500275cebce0e761525351ce591ae06624e9
lgbadluck <lg.badluck@gmail.com>
2024-02-23 12:31:26 +0200

    100% in Judge for WordFilter_04.java

--- ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsLab/WordFilter_04.java
+++ ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsLab/WordFilter_04.java
@@ -1,11 +1,17 @@
 package MapsLambdaStreamAPIsLab;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class WordFilter_04 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] words = Arrays.stream(scanner.nextLine().split(" "))
+                .filter(w -> w.length() % 2 == 0)
+                .toArray(String[]::new);
+        for (String word : words) {
+            System.out.println(word);
+        }
     }
 }
