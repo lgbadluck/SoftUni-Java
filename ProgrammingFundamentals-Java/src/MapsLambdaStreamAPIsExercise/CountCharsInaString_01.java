eb5b7f03eff41d6a338bbe1018fbe7473ae2b116
lgbadluck <lg.badluck@gmail.com>
2024-02-23 15:04:56 +0200

    100% in Judge for CountCharsInaString_01.java

--- ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsExercise/CountCharsInaString_01.java
+++ ProgrammingFundamentals-Java/src/MapsLambdaStreamAPIsExercise/CountCharsInaString_01.java
@@ -1,11 +1,29 @@
 package MapsLambdaStreamAPIsExercise;
 
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CountCharsInaString_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        LinkedHashMap<Character, Integer> charsMap = new LinkedHashMap<>();
+
+        String input = scanner.nextLine();
+
+        for (int i = 0; i < input.length(); i++) {
+            char thisChar = input.charAt(i);
+            if (thisChar != ' ') {
+                if (!charsMap.containsKey(thisChar)) {
+                    charsMap.put(thisChar, 0);
+                }
+                charsMap.put(thisChar, charsMap.get(thisChar) + 1);
+            }
+        }
+
+        for (Map.Entry<Character, Integer> entry : charsMap.entrySet()) {
+            System.out.printf("%c -> %d\n", entry.getKey(), entry.getValue());
+        }
     }
 }
