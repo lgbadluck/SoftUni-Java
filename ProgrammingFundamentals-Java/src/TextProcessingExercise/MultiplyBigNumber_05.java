eb34caad3d27f81b49ae0da99b7f36e3a9a6ec5e
lgbadluck <lg.badluck@gmail.com>
2024-03-09 19:38:03 +0200

    100% in Judge for MultiplyBigNumber_05.java

--- ProgrammingFundamentals-Java/src/TextProcessingExercise/MultiplyBigNumber_05.java
+++ ProgrammingFundamentals-Java/src/TextProcessingExercise/MultiplyBigNumber_05.java
@@ -1,11 +1,18 @@
 package TextProcessingExercise;
 
+import java.math.BigInteger;
 import java.util.Scanner;
 
 public class MultiplyBigNumber_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String firstNumber = scanner.nextLine();
+        String secondNumber = scanner.nextLine();
+
+        BigInteger num1 = new BigInteger(firstNumber);
+        BigInteger num2 = new BigInteger(secondNumber);
+
+        System.out.println(num1.multiply(num2));
     }
 }
