110fddf9819b6a43ac79a126949486027b2189dd
lgbadluck <lg.badluck@gmail.com>
2024-03-09 20:20:02 +0200

    100% in Judge for ValidUsernames_01.java

--- ProgrammingFundamentals-Java/src/TextProcessingExercise/ValidUsernames_01.java
+++ ProgrammingFundamentals-Java/src/TextProcessingExercise/ValidUsernames_01.java
@@ -6,6 +6,20 @@ public class ValidUsernames_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] input = scanner.nextLine().split(", ");
+
+        boolean isValidUser;
+        for (String word : input) {
+            if (word.length() < 3 || word.length() > 16) continue;
+            isValidUser = true;
+            for (char ch : word.toUpperCase().toCharArray()) {
+                if (!(ch >= 'A' && ch <= 'Z' || ch >= '0' && ch <= '9' || ch == '-' || ch == '_')) {
+                    isValidUser = false;
+                    break;
+                }
+            }
+            if (isValidUser)
+                System.out.println(word);
+        }
     }
 }
