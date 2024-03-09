8d3892a5d081321b1609c7b0c60899fadbbb477d
lgbadluck <lg.badluck@gmail.com>
2024-03-09 20:30:24 +0200

    100% in Judge for LettersChangeNumbers_08.java

--- ProgrammingFundamentals-Java/src/TextProcessingExercise/LettersChangeNumbers_08.java
+++ ProgrammingFundamentals-Java/src/TextProcessingExercise/LettersChangeNumbers_08.java
@@ -6,6 +6,37 @@ public class LettersChangeNumbers_08 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] codes = scanner.nextLine().split("\\s+");
+
+        double sum = 0;
+
+        for (String code : codes) {
+            double result = calculateCodeResult(code);
+            sum += result;
+        }
+
+        System.out.printf("%.2f", sum);
+    }
+
+    private static double calculateCodeResult(String code) {
+        char letterBefore = code.charAt(0);
+        double number = Double.parseDouble(code.substring(1, code.length() - 1));
+        char letterAfter = code.charAt(code.length() - 1);
+
+        //Before
+        if (Character.isUpperCase(letterBefore)) {
+            number /= letterBefore - 'A' + 1;
+        } else {
+            number *= letterBefore - 'a' + 1;
+        }
+
+        //After
+        if (Character.isUpperCase(letterAfter)) {
+            number -= letterAfter - 'A' + 1;
+        } else {
+            number += letterAfter - 'a' + 1;
+        }
+
+        return number;
     }
 }
