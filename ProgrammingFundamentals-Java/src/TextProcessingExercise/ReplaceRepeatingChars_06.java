36aa25589e4b0a8c692b68f02f94234201e53da6
lgbadluck <lg.badluck@gmail.com>
2024-03-09 19:44:20 +0200

    100% in Judge for ReplaceRepeatingChars_06.java

--- ProgrammingFundamentals-Java/src/TextProcessingExercise/ReplaceRepeatingChars_06.java
+++ ProgrammingFundamentals-Java/src/TextProcessingExercise/ReplaceRepeatingChars_06.java
@@ -6,6 +6,21 @@ public class ReplaceRepeatingChars_06 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+
+        for (int i = 0; i < input.length()-1; i++) {
+            char thisChar = input.charAt(i);
+            char nextChar = input.charAt(i+1);
+
+            if(thisChar != nextChar) {
+                System.out.print(thisChar);
+            }
+            if(i == input.length()-2 && thisChar != nextChar) {
+                System.out.print(nextChar);
+            }
+            if(i == input.length()-2 && thisChar == nextChar) {
+                System.out.print(nextChar);
+            }
+        }
     }
 }
