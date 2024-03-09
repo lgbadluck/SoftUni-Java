65b0912c4c1132fdef181d145f25b759e5a95090
lgbadluck <lg.badluck@gmail.com>
2024-03-09 19:34:56 +0200

    100% in Judge for DigitsLettersAndOther_05.java

--- ProgrammingFundamentals-Java/src/TextProcessingLab/DigitsLettersAndOther_05.java
+++ ProgrammingFundamentals-Java/src/TextProcessingLab/DigitsLettersAndOther_05.java
@@ -1,11 +1,35 @@
 package TextProcessingLab;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class DigitsLettersAndOther_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+
+        char[][] separated = new char[3][input.length()];
+
+        int countDigit = 0;
+        int countLetter = 0;
+        int countOther = 0;
+
+        for (char symbol : input.toCharArray()) {
+            if(Character.isDigit(symbol))
+                separated[0][countDigit++]=symbol;
+            else if (Character.isLetter(symbol))
+                separated[1][countLetter++]=symbol;
+            else
+                separated[2][countOther++]=symbol;
+        }
+
+        System.out.println(Arrays.deepToString(separated).trim()
+                .replace("], ","\n")
+                .replace("[","")
+                .replace("]","")
+                .replace(", ","")
+                .replace("\0","")
+        );
     }
 }
