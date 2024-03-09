40832c2e12ccb97e5b92a8ddae782295b9e98253
lgbadluck <lg.badluck@gmail.com>
2024-03-09 20:10:17 +0200

    100% in Judge for CharacterMultiplier_02.java

--- ProgrammingFundamentals-Java/src/TextProcessingExercise/CharacterMultiplier_02.java
+++ ProgrammingFundamentals-Java/src/TextProcessingExercise/CharacterMultiplier_02.java
@@ -6,6 +6,24 @@ public class CharacterMultiplier_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] input  = scanner.nextLine().split(" ");
+
+        int minLength = Math.min(input[0].length(), input[1].length());
+
+        int sumCharCodes = 0;
+        for (int i = 0; i < minLength; i++) {
+            sumCharCodes+=input[0].charAt(i) * input[1].charAt(i);
+        }
+
+        if (input[0].length() > minLength) {
+            for (int i = minLength; i < input[0].length(); i++) {
+                sumCharCodes+=input[0].charAt(i);
+            }
+        } else {
+            for (int i = minLength; i < input[1].length(); i++) {
+                sumCharCodes+=input[1].charAt(i);
+            }
+        }
+        System.out.println(sumCharCodes);
     }
 }
