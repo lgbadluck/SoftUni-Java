46294a6a223d576ed79401109a5f44ce0cee33c8
lgbadluck <lg.badluck@gmail.com>
2024-03-09 20:03:23 +0200

    100% in Judge for ExtractFile_03.java

--- ProgrammingFundamentals-Java/src/TextProcessingExercise/ExtractFile_03.java
+++ ProgrammingFundamentals-Java/src/TextProcessingExercise/ExtractFile_03.java
@@ -6,6 +6,11 @@ public class ExtractFile_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String[] input = scanner.nextLine().split("\\\\");
+        String fileName = input[input.length-1].split("\\.")[0];
+        String extName = input[input.length-1].split("\\.")[1];
+
+        System.out.println("File name: " + fileName);
+        System.out.println("File extension: " + extName);
     }
 }
