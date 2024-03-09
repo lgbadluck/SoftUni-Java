f90af8d1977d5971ca8b5759c31a6abefc86e443
lgbadluck <lg.badluck@gmail.com>
2024-03-09 19:55:51 +0200

    100% in Judge for StringExplosion_07.java

--- ProgrammingFundamentals-Java/src/TextProcessingExercise/StringExplosion_07.java
+++ ProgrammingFundamentals-Java/src/TextProcessingExercise/StringExplosion_07.java
@@ -6,6 +6,20 @@ public class StringExplosion_07 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+
+        int explosionCounter = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char thisChar = input.charAt(i);
+
+            if (thisChar != '>' && explosionCounter == 0) {
+                System.out.print(thisChar);
+            } else if (thisChar != '>' && explosionCounter > 0) {
+                explosionCounter--;
+            } else {
+                System.out.print(thisChar);
+                explosionCounter += Integer.parseInt(String.valueOf(input.charAt(i + 1)));
+            }
+        }
     }
 }
