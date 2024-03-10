4d76bbc1d8cb8e8a4640557d8359f6d29fb1daca
lgbadluck <lg.badluck@gmail.com>
2024-03-10 08:00:16 +0200

    100% in Judge for ExtractPersonInformation_01.java

--- ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/ExtractPersonInformation_01.java
+++ ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/ExtractPersonInformation_01.java
@@ -7,5 +7,25 @@ public class ExtractPersonInformation_01 {
         Scanner scanner = new Scanner(System.in);
 
         int n = Integer.parseInt(scanner.nextLine());
+
+        while (n > 0) {
+            String input = scanner.nextLine();
+
+            String name, age;
+            int nameStartIdx, nameEndIdx;
+            int ageStartIdx, ageEndIdx;
+
+            nameStartIdx = input.indexOf('@')+1;
+            nameEndIdx = input.indexOf('|');
+            ageStartIdx = input.indexOf('#')+1;
+            ageEndIdx = input.indexOf('*');
+
+            name = input.substring(nameStartIdx, nameEndIdx);
+            age = input.substring(ageStartIdx, ageEndIdx);
+
+            System.out.println(name + " is " + age + " years old.");
+
+            n--;
+        }
     }
 }
