655fa135390728aec5a519a766d0d993d9a43673
lgbadluck <lg.badluck@gmail.com>
2024-03-10 08:54:42 +0200

    100% in Judge for HTML_05.java

--- ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/HTML_05.java
+++ ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/HTML_05.java
@@ -6,6 +6,16 @@ public class HTML_05 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String titleName = scanner.nextLine();
+        System.out.printf("<h1>\n\t%s\n</h1>\n", titleName);
+        String articleContent = scanner.nextLine();
+        System.out.printf("<article>\n\t%s\n</article>\n", articleContent);
+
+        String inputComment = scanner.nextLine();
+        while (!inputComment.equals("end of comments")) {
+            System.out.printf("<div>\n\t%s\n</div>\n", inputComment);
+            inputComment = scanner.nextLine();
+        }
+
     }
 }
