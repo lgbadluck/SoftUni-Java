50ffc57ee69941b57e3930b0e624424b6d78b075
lgbadluck <lg.badluck@gmail.com>
2024-03-10 08:05:53 +0200

    100% in Judge for AsciiSumator_02.java

--- ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/AsciiSumator_02.java
+++ ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/AsciiSumator_02.java
@@ -6,6 +6,17 @@ public class AsciiSumator_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        char start = scanner.nextLine().charAt(0);
+        char end = scanner.nextLine().charAt(0);
+        String text = scanner.nextLine();
+
+        int sum = 0;
+
+        for (char symbol : text.toCharArray()) {
+            if (symbol > start && symbol < end) {
+                sum += symbol;
+            }
+        }
+        System.out.println(sum);
     }
 }
