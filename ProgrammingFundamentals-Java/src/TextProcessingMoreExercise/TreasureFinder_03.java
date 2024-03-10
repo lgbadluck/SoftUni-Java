02a7abc1cf360a41c68cb5b26357ae6f914156ee
lgbadluck <lg.badluck@gmail.com>
2024-03-10 08:37:05 +0200

    100% in Judge for TreasureFinder_03.java

--- ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/TreasureFinder_03.java
+++ ProgrammingFundamentals-Java/src/TextProcessingMoreExercise/TreasureFinder_03.java
@@ -1,11 +1,43 @@
 package TextProcessingMoreExercise;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class TreasureFinder_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        int[] keySeq = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt).toArray();
+
+        String input = scanner.nextLine();
+
+        while (!input.equals("find")) {
+            char[] toDecrypt = input.toCharArray();
+
+            for (int i = 0; i < toDecrypt.length; i++) {
+                toDecrypt[i] = (char) (toDecrypt[i] - keySeq[i % keySeq.length]);
+            }
+
+
+            String decrypted = String.valueOf(toDecrypt);
+            //System.out.println(decrypted);
+
+            String type, coordinates;
+            int typeStart, typeEnd;
+            typeStart = decrypted.indexOf('&') + 1;
+            typeEnd = decrypted.lastIndexOf('&');
+
+            int coordinatesStart, coordinateEnd;
+            coordinatesStart = decrypted.indexOf('<') + 1;
+            coordinateEnd = decrypted.lastIndexOf('>');
+
+            type = decrypted.substring(typeStart, typeEnd);
+            coordinates = decrypted.substring(coordinatesStart, coordinateEnd);
+
+            System.out.println("Found " + type + " at " + coordinates);
+
+            input = scanner.nextLine();
+        }
     }
 }
