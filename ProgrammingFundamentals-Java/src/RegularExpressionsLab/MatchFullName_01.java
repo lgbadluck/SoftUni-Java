0db1c80141ec391a0f14e4a5448e26261ee9f062
lgbadluck <lg.badluck@gmail.com>
2024-03-17 08:45:11 +0200

    100% in Judge for MatchFullName_01.java

--- ProgrammingFundamentals-Java/src/RegularExpressionsLab/MatchFullName_01.java
+++ ProgrammingFundamentals-Java/src/RegularExpressionsLab/MatchFullName_01.java
@@ -1,11 +1,21 @@
 package RegularExpressionsLab;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MatchFullName_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+        String regex = "\\b[A-Z][a-z]+ [A-Z][a-z]+";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while(matcher.find()) {
+            System.out.print(matcher.group() + " ");
+        }
     }
 }
