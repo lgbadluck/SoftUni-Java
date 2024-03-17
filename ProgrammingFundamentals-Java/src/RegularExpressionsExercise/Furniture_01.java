4b11fbc0188ef96b091e770c67146092e7f52876
lgbadluck <lg.badluck@gmail.com>
2024-03-17 09:09:12 +0200

    100% in Judge for Furniture_01.java

--- ProgrammingFundamentals-Java/src/RegularExpressionsExercise/Furniture_01.java
+++ ProgrammingFundamentals-Java/src/RegularExpressionsExercise/Furniture_01.java
@@ -1,11 +1,37 @@
 package RegularExpressionsExercise;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Furniture_01 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String regex = "[>]{2}(?<productName>\\w+)[<]{2}(?<price>\\d+[.]?\\d*)!(?<quantity>\\d+)";
+        String input = scanner.nextLine();
+
+        Pattern pattern = Pattern.compile(regex);
+
+        double totalSum = 0;
+        List<String> products = new LinkedList<>();
+
+        while (!input.equals("Purchase")) {
+            Matcher matcher = pattern.matcher(input);
+
+            if (matcher.find()) {
+                totalSum += Double.parseDouble(matcher.group("price")) * Double.parseDouble(matcher.group("quantity"));
+                products.add(matcher.group("productName"));
+            }
+
+            input = scanner.nextLine();
+        }
+        System.out.println("Bought furniture:");
+        for (String product : products) {
+            System.out.println(product);
+        }
+        System.out.printf("Total money spend: %.2f\n", totalSum);
     }
 }
