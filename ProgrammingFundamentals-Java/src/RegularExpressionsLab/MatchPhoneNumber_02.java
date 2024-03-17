4ad85d8e96178290ab6f7f4320893bc7ca8e6605
lgbadluck <lg.badluck@gmail.com>
2024-03-17 08:51:54 +0200

    100% in Judge for MatchPhoneNumber_02.java

--- ProgrammingFundamentals-Java/src/RegularExpressionsLab/MatchPhoneNumber_02.java
+++ ProgrammingFundamentals-Java/src/RegularExpressionsLab/MatchPhoneNumber_02.java
@@ -1,11 +1,26 @@
 package RegularExpressionsLab;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MatchPhoneNumber_02 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+        String regex = "\\+359([ -])2\\1\\d{3}\\1\\d{4}\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        List<String> matchedPhones = new LinkedList<>();
+        while(matcher.find()) {
+            matchedPhones.add(matcher.group());
+        }
+
+        System.out.println(String.join(", ", matchedPhones));
     }
 }
