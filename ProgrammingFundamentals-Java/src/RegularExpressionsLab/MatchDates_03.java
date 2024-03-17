80feb359d101d9fbfb6876e0492683f1afd43234
lgbadluck <lg.badluck@gmail.com>
2024-03-17 08:56:50 +0200

    100% in Judge for MatchDates_03.java

--- ProgrammingFundamentals-Java/src/RegularExpressionsLab/MatchDates_03.java
+++ ProgrammingFundamentals-Java/src/RegularExpressionsLab/MatchDates_03.java
@@ -1,11 +1,26 @@
 package RegularExpressionsLab;
 
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MatchDates_03 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int n = Integer.parseInt(scanner.nextLine());
+        String input = scanner.nextLine();
+        String regex = "(?<day>\\d{2})([-./])(?<month>[A-Z][a-z]{2})\\2(?<year>\\d{4})\\b";
+
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(input);
+
+        while(matcher.find()) {
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
+
+            System.out.printf("Day: %s, Month: %s, Year: %s\n", day, month, year);
+        }
+
     }
 }
