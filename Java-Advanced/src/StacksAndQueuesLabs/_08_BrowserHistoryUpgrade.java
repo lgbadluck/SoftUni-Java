a74bd812f3b571321e7fd41b3c9b5e56db7efe2f
lgbadluck <lg.badluck@gmail.com>
2024-05-26 13:21:11 +0300

    100% in Judge for _08_BrowserHistoryUpgrade.java

---
+++ Java-Advanced/src/StacksAndQueuesLabs/_08_BrowserHistoryUpgrade.java
@@ -0,0 +1,38 @@
+package StacksAndQueuesLabs;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class _08_BrowserHistoryUpgrade {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        ArrayDeque<String> forwardPages = new ArrayDeque<>();
+        ArrayDeque<String> browserHistory = new ArrayDeque<>();
+
+        String command = scanner.nextLine();
+        while (!command.equals("Home")) {
+            if (command.equals("back")) {
+                String output = "no previous URLs";
+                if (browserHistory.size() >= 2) {
+                    forwardPages.addFirst(browserHistory.peek());
+                    browserHistory.pop();
+                    output = browserHistory.peek();
+                }
+                System.out.println(output);
+            } else if (command.equals("forward")) {
+                String output = "no next URLs";
+                if (!forwardPages.isEmpty()) {
+                    output = forwardPages.peek();
+                    browserHistory.push(forwardPages.pop());
+                }
+                System.out.println(output);
+            } else {
+                System.out.println(command);
+                browserHistory.push(command);
+                forwardPages.clear();
+            }
+            command = scanner.nextLine();
+        }
+    }
+}
+
