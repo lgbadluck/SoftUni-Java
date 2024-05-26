fea14dc9b6b23c85a10aff59a74546ec57c40b36
lgbadluck <lg.badluck@gmail.com>
2024-05-26 17:52:25 +0300

    100% in Judge for _03_PeriodicTable.java

---
+++ Java-Advanced/src/SetsAndMapsAdvancedExercises/_03_PeriodicTable.java
@@ -0,0 +1,27 @@
+package SetsAndMapsAdvancedExercises;
+
+import java.util.List;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class _03_PeriodicTable {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int n = Integer.parseInt(scanner.nextLine());
+
+        Set<String> chemicalElements = new TreeSet<>();
+
+        for (int i = 0; i < n; i++) {
+            //"Mo O Ce"
+            String compound = scanner.nextLine();
+            // ["Mo", "O", "Ce"]
+            String[] elements = compound.split(" ");
+            chemicalElements.addAll(List.of(elements));
+        }
+
+        chemicalElements.forEach(element -> System.out.print(element + " "));
+    }
+}
