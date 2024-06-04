f4e266fc40fa38d5d607dd440772c7e35de8797b
lgbadluck <lg.badluck@gmail.com>
2024-06-04 22:44:28 +0300

    100% in Judge for FunctionalProgramming Lab&Exercises all problems

---
+++ Java-Advanced/src/FunctionalProgrammingExercises/_02_SumNumbers.java
@@ -0,0 +1,17 @@
+package FunctionalProgrammingExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class _02_SumNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] arr = Arrays.stream(scanner.nextLine().split(", "))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        System.out.println("Count = " + arr.length);
+        System.out.println("Sum = " + Arrays.stream(arr).sum());
+    }
+}
---
+++ Java-Advanced/src/FunctionalProgrammingExercises/_06_FindEvensOrOdds.java
@@ -0,0 +1,29 @@
+package FunctionalProgrammingExercises;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.IntPredicate;
+import java.util.stream.IntStream;
+
+public class _06_FindEvensOrOdds {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] range = Arrays.stream(scanner.nextLine().split("\\s+"))
+                .mapToInt(Integer::parseInt)
+                .toArray();
+
+        int begin = range[0];
+        int end = range[1];
+
+        String oddOrEven = scanner.nextLine();
+
+        IntPredicate predicate = oddOrEven.equals("even")
+                ? x -> x % 2 == 0
+                : x -> x % 2 != 0;
+
+        IntStream.rangeClosed(begin, end)
+                .filter(predicate)
+                .forEach(e -> System.out.print(e + " "));
+    }
+}
