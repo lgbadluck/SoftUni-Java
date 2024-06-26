4543d40166383d4c01dc7a42c82c76e040b3d4b4
lgbadluck <lg.badluck@gmail.com>
2024-06-26 16:10:13 +0300

    100% in AlphaJudge for _04_TrafficLights

---
+++ Java-OOP/src/_01_WorkingWithAbstractionExercise/_04_TrafficLights/Light.java
@@ -0,0 +1,8 @@
+package _01_WorkingWithAbstractionExercise._04_TrafficLights;
+
+public enum Light {
+
+    RED,
+    GREEN,
+    YELLOW;
+}
--- Java-OOP/src/_01_WorkingWithAbstractionExercise/_05_JediGalaxy/Main.java
+++ Java-OOP/src/_01_WorkingWithAbstractionExercise/_05_JediGalaxy/Main.java
@@ -1,4 +1,4 @@
-package workingWithAbstraction.jediGalaxy;
+package _01_WorkingWithAbstractionExercise._05_JediGalaxy;
 
 import java.util.Arrays;
 import java.util.Scanner;
