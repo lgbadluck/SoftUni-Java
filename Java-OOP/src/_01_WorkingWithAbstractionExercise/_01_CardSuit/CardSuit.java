5e2e58bf999ed8188681f844be32e09fc004e9d1
lgbadluck <lg.badluck@gmail.com>
2024-06-26 15:57:24 +0300

    100% in AlphaJudge for _01_CardSuit

---
+++ Java-OOP/src/_01_WorkingWithAbstractionExercise/_01_CardSuit/CardSuit.java
@@ -0,0 +1,13 @@
+package _01_WorkingWithAbstractionExercise._01_CardSuit;
+
+
+public enum CardSuit {
+
+    // Енъм стойности
+    // Ordinal value -> поредна стойност/позиция на енъм стойността
+    CLUBS,
+    DIAMONDS,
+    HEARTS,
+    SPADES;
+
+}
