5a8a9cd37f2bea3b214cf2f23c8432ad4629193e
lgbadluck <lg.badluck@gmail.com>
2024-06-26 15:59:43 +0300

    100% in AlphaJudge for _02_CardRank

---
+++ Java-OOP/src/_01_WorkingWithAbstractionExercise/_02_CardRank/CardRank.java
@@ -0,0 +1,18 @@
+package _01_WorkingWithAbstractionExercise._02_CardRank;
+
+public enum CardRank {
+
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
