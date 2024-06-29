1bd04b280a4a42451ea93b9c884548476544c1fe
lgbadluck <lg.badluck@gmail.com>
2024-06-29 19:28:54 +0300

    100% in AlphaJudge for _01_RhombusOfStars

---
+++ Java-OOP/src/_01_WorkingWithAbstractionLab/_01_RhombusOfStars/Main.java
@@ -0,0 +1,36 @@
+package _01_WorkingWithAbstractionLab._01_RhombusOfStars;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+
+        printTopPart(size);
+        printBottomPart(size);
+    }
+
+    private static void printBottomPart(int size) {
+        for (int r = 1; r < size; r++) {
+            printRepeatingString(" ", r);
+            printRepeatingString("* ", size - r);
+            System.out.println();
+        }
+    }
+
+    private static void printTopPart(int n) {
+        for (int r = 1; r <= n; r++) {
+            printRepeatingString(" ", n - r);
+            printRepeatingString("* ", r);
+            System.out.println();
+        }
+    }
+
+    private static void printRepeatingString(String str, int repetitions) {
+        for (int i = 0; i < repetitions; i++) {
+            System.out.print(str);
+        }
+    }
+}
