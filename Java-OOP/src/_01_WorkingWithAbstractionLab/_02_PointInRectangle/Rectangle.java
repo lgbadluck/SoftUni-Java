05b3c3daaa1f159e1b034c795fa45427b1648ef4
lgbadluck <lg.badluck@gmail.com>
2024-06-29 19:33:02 +0300

    100% in AlphaJudge for _02_PointInRectangle

---
+++ Java-OOP/src/_01_WorkingWithAbstractionLab/_02_PointInRectangle/Rectangle.java
@@ -0,0 +1,16 @@
+package _01_WorkingWithAbstractionLab._02_PointInRectangle;
+
+public class Rectangle {
+    private final Point a;
+    private final Point b;
+
+    public Rectangle(Point a, Point b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public boolean contains(Point p) {
+        return p.getX() >= a.getX() && p.getX() <= b.getX()
+                && p.getY() >= a.getY() && p.getY() <= b.getY();
+    }
+}
