9ad15e9fa8e43680e197f17cf91727429408d057
lgbadluck <lg.badluck@gmail.com>
2024-06-29 19:38:08 +0300

    100% in AlphaJudge for _03_StudentSystem

---
+++ Java-OOP/src/_01_WorkingWithAbstractionLab/_03_StudentSystem/Student.java
@@ -0,0 +1,22 @@
+package _01_WorkingWithAbstractionLab._03_StudentSystem;
+
+public record Student(String name, int age, double grade) {
+
+    @Override
+    public String toString() {
+        return String.format("%s is %s years old. %s", name, age, getPostfix());
+    }
+
+    private String getPostfix() {
+        String postfix = "Very nice person.";
+
+        if (grade >= 5.00) {
+            postfix = "Excellent student.";
+        }
+        else if (grade >= 3.50) {
+            postfix = "Average student.";
+        }
+
+        return postfix;
+    }
+}
