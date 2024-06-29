8ca8d5d21845a6d315a028856381355d67e7360c
lgbadluck <lg.badluck@gmail.com>
2024-06-29 19:42:39 +0300

    100% in AlphaJudge for _04_HotelReservation

---
+++ Java-OOP/src/_01_WorkingWithAbstractionLab/_04_HotelReservation/Season.java
@@ -0,0 +1,18 @@
+package _01_WorkingWithAbstractionLab._04_HotelReservation;
+
+public enum Season {
+    SPRING(2),
+    SUMMER(4),
+    AUTUMN(1),
+    WINTER(3);
+
+    private final int multiplier;
+
+    Season(int multiplier) {
+        this.multiplier = multiplier;
+    }
+
+    public int getMultiplier() {
+        return multiplier;
+    }
+}
