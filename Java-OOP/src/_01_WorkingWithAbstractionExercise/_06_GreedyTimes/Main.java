b003aa575161a65c240610306d75a170b8e6bf30
lgbadluck <lg.badluck@gmail.com>
2024-06-29 19:26:08 +0300

    100% in AlphaJudge for _06_GreedyTimes

--- Java-OOP/src/_01_WorkingWithAbstractionExercise/_06_GreedyTimes/Main.java
+++ Java-OOP/src/_01_WorkingWithAbstractionExercise/_06_GreedyTimes/Main.java
@@ -1,76 +1,69 @@
-
 package _01_WorkingWithAbstractionExercise._06_GreedyTimes;
 
-
-import java.util.Arrays;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
+        long bagMaxCapacity = Long.parseLong(scanner.nextLine());
+        String[] safe = scanner.nextLine().split("\\s+");
 
-        int[] dimensions = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-        int rows = dimensions[0];
-        int cols = dimensions[1];
+        LinkedHashMap<String, LinkedHashMap<String, Long>> bag = new LinkedHashMap<>();
+        long goldAmount = 0;
+        long gemstonesAmount = 0;
+        long cashAmount = 0;
 
-        int[][] matrix = new int[rows][cols];
-        fillTheMatrix(rows, cols, matrix);
+        for (int i = 0; i < safe.length; i += 2) {
+            String name = safe[i];
+            long amount = Long.parseLong(safe[i + 1]);
 
-        long totalStars = 0;
+            if (amount + cashAmount + gemstonesAmount + goldAmount > bagMaxCapacity) {
+                continue;
+            }
 
-        String command = scanner.nextLine();
-        while (!command.equals("Let the Force be with you")) {
+            String element = "";
 
-            int[] jediCoordinates = Arrays.stream(command.split(" ")).mapToInt(Integer::parseInt).toArray();
-            int[] evilCoordinates = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
-            // Кординати на лошият човек
-            int evilRow = evilCoordinates[0];
-            int evilCol = evilCoordinates[1];
-            moveEvilPlayer(evilRow, evilCol, matrix);
+            if (name.length() == 3) {
+                element = "Cash";
+                if (gemstonesAmount < amount + cashAmount) {
+                    continue;
+                }
+            } else if (name.toLowerCase().endsWith("gem")) {
+                element = "Gem";
+                if (goldAmount < amount + gemstonesAmount) {
+                    continue;
+                }
+            } else if (name.toLowerCase().equals("gold")) {
+                element = "Gold";
+            } else continue;
 
-            // Кординати да джедая
-            int jediRow = jediCoordinates[0];
-            int jediCol = jediCoordinates[1];
-            totalStars = moveJediPlayerAndCollectStars(jediRow, jediCol, matrix, totalStars);
+            bag.putIfAbsent((element), new LinkedHashMap<>());
+            bag.get(element)
+                    .putIfAbsent(name, 0L);
+            long prevAmount = bag.get(element).get(name);
+            bag.get(element)
+                    .put(name, prevAmount + amount);
 
-            command = scanner.nextLine();
+            switch (element) {
+                case "Cash" -> cashAmount += amount;
+                case "Gold" -> goldAmount += amount;
+                case "Gem" -> gemstonesAmount += amount;
+            }
+
         }
 
-        System.out.println(totalStars);
-    }
+        for (Map.Entry<String, LinkedHashMap<String, Long>> entry : bag.entrySet()) {
+            Long sumValues = entry.getValue().values().stream().mapToLong(l -> l).sum();
+            System.out.printf("<%s> $%s\n", entry.getKey(), sumValues);
 
-    private static long moveJediPlayerAndCollectStars(int jediRow, int jediCol, int[][] matrix, long totalStars) {
-
-        while (jediRow >= 0 && jediCol < matrix[1].length) {
-            if (jediRow < matrix.length && jediCol >= 0 && jediCol < matrix[0].length) {
-                totalStars += matrix[jediRow][jediCol];
-            }
-            jediCol++;
-            jediRow--;
-        }
-        return totalStars;
-    }
-
-    private static void moveEvilPlayer(int evilRow, int evilCol, int[][] matrix) {
-
-        while (evilRow >= 0 && evilCol >= 0) {
-            if (evilRow < matrix.length && evilCol < matrix[0].length) {
-                matrix[evilRow][evilCol] = 0;
-            }
-            evilRow--;
-            evilCol--;
-        }
-    }
-
-    private static void fillTheMatrix(int rows, int cols, int[][] matrix) {
-
-        int value = 0;
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                matrix[row][col] = value++;
-            }
+            entry.getValue().entrySet()
+                    .stream()
+                    .sorted((e1, e2) -> e2.getKey()
+                            .compareTo(e1.getKey()))
+                    .forEach(item -> System.out.printf("##%s - %d\n", item.getKey(), item.getValue()));
         }
     }
 }
