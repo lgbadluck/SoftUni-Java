b66bc9532e51877d2fb21391c675317a06d9fb5f
lgbadluck <lg.badluck@gmail.com>
2024-06-30 14:48:20 +0300

    100% in AlphaJudge for _01_Person

---
+++ Java-OOP/src/_03_InheritanceExercise/_01_Person/Child.java
@@ -0,0 +1,7 @@
+package _03_InheritanceExercise._01_Person;
+
+public class Child extends Person {
+    public Child(String name, int age) {
+        super(name, age);
+    }
+}
---
+++ Java-OOP/src/_03_InheritanceExercise/_01_Person/Person.java
@@ -0,0 +1,19 @@
+package _03_InheritanceExercise._01_Person;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
