4f12e4339b5108d08f20608ea33546987e56c656
lgbadluck <lg.badluck@gmail.com>
2024-07-07 13:18:52 +0300

    100% in AlphaJudge for _03_SayHello

---
+++ Java-OOP/src/_04_InterfacesAndAbstractionLab/_03_SayHello/Person.java
@@ -0,0 +1,12 @@
+package _04_InterfacesAndAbstractionLab._03_SayHello;
+
+
+public interface Person {
+
+    //+getName(): String
+    //+sayHello(): String
+
+    String getName();
+
+    String sayHello();
+}
