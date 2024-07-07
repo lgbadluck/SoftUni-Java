214c23dade73b21c2266f4e7d95166cf6716e128
lgbadluck <lg.badluck@gmail.com>
2024-07-07 16:11:26 +0300

    100% in AlphaJudge for _03_BirthdayCelebration

---
+++ Java-OOP/src/_04_InterfacesAndAbstractionExercise/_03_BirthdayCelebration/Birthable.java
@@ -0,0 +1,5 @@
+package _04_InterfacesAndAbstractionExercise._03_BirthdayCelebration;
+
+public interface Birthable {
+    String getBirthDate();
+}
---
+++ Java-OOP/src/_04_InterfacesAndAbstractionExercise/_03_BirthdayCelebration/Person.java
@@ -0,0 +1,6 @@
+package _04_InterfacesAndAbstractionExercise._03_BirthdayCelebration;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
