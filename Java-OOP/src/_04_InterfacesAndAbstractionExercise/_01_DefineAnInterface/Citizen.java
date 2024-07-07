f3745afe6fc5ba6e1dcd84c35a2ff848c5b543ec
lgbadluck <lg.badluck@gmail.com>
2024-07-07 15:37:25 +0300

    100% in AlphaJudge for _01_DefineAnInterface

---
+++ Java-OOP/src/_04_InterfacesAndAbstractionExercise/_01_DefineAnInterface/Citizen.java
@@ -0,0 +1,21 @@
+package _04_InterfacesAndAbstractionExercise._01_DefineAnInterface;
+
+public class Citizen implements Person {
+    private String name;
+    private int age;
+
+    public Citizen(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public int getAge() {
+        return age;
+    }
+}
---
+++ Java-OOP/src/_04_InterfacesAndAbstractionExercise/_01_DefineAnInterface/Person.java
@@ -0,0 +1,6 @@
+package _04_InterfacesAndAbstractionExercise._01_DefineAnInterface;
+
+public interface Person {
+    String getName();
+    int getAge();
+}
