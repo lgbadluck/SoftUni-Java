43cb82de73c7092070bdc5e9f68dd9db13f67fe0
lgbadluck <lg.badluck@gmail.com>
2024-07-14 14:16:16 +0300

    100% in AlphaJudge for _01_Vehicles

---
+++ Java-OOP/src/_05_PolymorphismExercise/_01_Vehicles/Car.java
@@ -0,0 +1,13 @@
+package _05_PolymorphismExercise._01_Vehicles;
+
+public class Car extends Vehicle {
+
+    public Car(double fuelQuantity, double fuelCostPerKm) {
+        super(fuelQuantity, fuelCostPerKm + 0.9);
+    }
+
+    @Override
+    public void refuel(double fuelToRefill) {
+        this.setFuelQuantity(this.getFuelQuantity() + fuelToRefill);
+    }
+}
